2850f6cd6ed06fd37e5741ab66f8ab2df94256e4
Korisnik <ernadglavinic@gmail.com>
2026-06-03 21:10:56 +0200

    Added bar char

--- script.js
+++ script.js
@@ -68,3 +68,45 @@ new Chart(pieChart, {
         ]
     }
 });
+
+const barChart = document.getElementById('barChart');
+
+const labels3 = [
+    '13 - 17',
+    '18 - 24',
+    '25 - 34',
+    '35 – 44',
+    '45 – 54',
+    '55 – 64'
+];
+
+new Chart(barChart, {  
+    type: 'bar',
+    data: {
+        labels: labels3,
+        datasets: [
+            {
+                label: 'Posjete po starosnim grupama',
+                data: [ 1.7, 43.3, 34.4, 13.6, 5.6, 1.2 ],
+                backgroundColor: [
+                    'rgba(255, 99, 133, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1
+
+            }
+        ]
+    }
+});
